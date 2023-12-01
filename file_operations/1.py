b4633aac32ed08207d725c2a8c2756eee771d086
guanfawang <guanfa_wang@apple.com>
2023-12-01 20:14:47 +0800

    new

---
+++ file_operations/1.py
@@ -0,0 +1,5 @@
+import os
+path1 = os.path.abspath('.') # 获取当前所处的文件夹绝对路径
+print(path1) # /Users/guanfawang/Python_Language/file_operations
+path2 = os.path.abspath('..') # 表示当前文件夹的上一级文件夹的绝对路径
+print(path2) # /Users/guanfawang/Python_Language
